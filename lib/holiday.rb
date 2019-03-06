2965079a703a7027d357ec1cdd9f75af54a5c068
favorable-vector-1743 <epzads@gmail.com>
2019-03-06 04:54:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 array = holiday_hash[:winter].values
-array.flatten
+return array.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
